d9a9934698c80f6d2be15d8f6ca3e670ff430f05
Helder-Lucau <helderlucau@gmail.com>
2023-09-28 00:13:55 +0300

    2 failed test

--- server/models.py
+++ server/models.py
@@ -14,6 +14,17 @@ class Author(db.Model):
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+    
+    @validates('name')
+    def validate_name(self, key, author):
+        if author == '':
+            raise ValueError("Name can not be empty")
+        return author
+    
+    @validates('phone_number')
+    def validate_phoene_number(self, key, phoneno):
+        if len(phoneno) != 10:
+            raise ValueError("Phone numbers are exactly ten digits")
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -30,3 +41,20 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Summary too long test. More than 250 chars.")
+        
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Summary is a maxiumum 250 characters")
+    
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        categories = ["Fiction", "Non-Fiction"]
+        if category not in categories:
+            raise ValueError("Post category is either Fiction or Non-Fiction")
